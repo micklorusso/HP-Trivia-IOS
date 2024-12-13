237c5b234695ccf54f4aa516a3ae07b435fd3951
milorusso <milorusso@deloitte.it>
2024-12-13 23:16:40 +0100

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Lorusso, Michele on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
